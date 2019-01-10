61ee3999e05f9efba425d27a176fbae10d6f842e
Oleksandr Yurchak <yurchachok@gmail.com>
2019-01-10 16:52:11 +0200

    lesson6 cycles next try

---
+++ src/lesson6/Cycles.java
@@ -0,0 +1,14 @@
+package lesson6;
+
+public class Cycles {
+    public static void main(String[] args) {
+        int x = 0;
+
+        for ( x = 0 ; x < 10 ; x++) {
+
+            System.out.println("s " + x);
+        }
+
+
+    }
+}
